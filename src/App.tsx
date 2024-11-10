87742d3c141b1a3fff5a0c3f6c62276f276c8658
Lucas Roig <lucas1.roig@gmail.com>
2024-11-10 13:02:28 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl">Test</div>
+      <Button>Test</Button>
+    </>
+  );
+}
+
+export default App;
