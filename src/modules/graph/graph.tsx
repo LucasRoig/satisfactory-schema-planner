50ca6d5e4c906419e0457755e9e86063b96854a0
Lucas Roig <lucas1.roig@gmail.com>
2024-11-10 18:13:59 +0100

    feat: add settings button

--- src/modules/graph/graph.tsx
+++ src/modules/graph/graph.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
-import { PlusCircle } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { PlusCircle, Settings } from "lucide-react";
 import { useProfileContext } from "../profile/profile-context";
 
 export function Graph() {
@@ -12,7 +13,8 @@ export function Graph() {
         <Skeleton className="w-[100px] h-[100px] rounded-full" />
       </div>
     );
-  } else if (profiles?.length === 0) {
+  }
+  if (profiles?.length === 0) {
     return (
       <div className="w-full h-full flex items-center justify-center">
         <Button onClick={openCreateProfileModal}>
@@ -25,4 +27,17 @@ export function Graph() {
   if (selectedProfile === undefined) {
     return <div className="w-full h-full flex items-center justify-center">Please select a profile</div>;
   }
+  return (
+    <div className="w-full h-full relative">
+      <SettingsButton className="absolute top-2 left-2" />
+    </div>
+  );
+}
+
+function SettingsButton({ className }: { className?: string }) {
+  return (
+    <Button variant="outline" className={cn("rounded-full", className)} size="icon">
+      <Settings className="h-5 w-5" />
+    </Button>
+  );
 }
