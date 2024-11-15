e032a46adbf2237bcf1ca8355faf6064f1f2a768
Lucas Roig <lucas1.roig@gmail.com>
2024-11-15 11:57:13 +0100

    feat: show item source name

--- src/modules/schema-drawer/nodes/source-node.tsx
+++ src/modules/schema-drawer/nodes/source-node.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { useProfileContext } from "@/modules/profile/profile-context";
 import type { NodeProps } from "@xyflow/react";
 import { BaseNode } from "./base-node";
 import { CustomHandle } from "./custom-handle";
@@ -6,13 +7,18 @@ import type { SourceNode as SourceNodeType } from "./nodes-types";
 import { RotationControls } from "./rotation-controls";
 
 export function SourceNode(props: NodeProps<SourceNodeType>) {
+  const { items } = useProfileContext();
+  const item = props.data.itemId !== undefined ? items.get(props.data.itemId) : undefined;
+  if (item === undefined && props.data.itemId !== undefined) {
+    throw new Error(`Item ${props.data.itemId} not found`);
+  }
   return (
     <BaseNode
       className={cn("aspect-square rounded-full flex flex-col items-center justify-center relative")}
       selected={props.selected}
     >
       {props.selected && <RotationControls {...props} />}
-      Source
+      {item ? `${props.data.quantity} ${item.name}` : "Source"}
       <CustomHandle type="source" position={props.data.orientation} />
     </BaseNode>
   );
